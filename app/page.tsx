1780ccefa9fba6ebbe5dc2a6dc3808963bc737d6
tokukiyu <tokkiyuam@gmail.com>
2024-01-14 00:50:55 +0300

    header updated

--- app/page.tsx
+++ app/page.tsx
@@ -27,9 +27,9 @@ export default function Home() {
       <Brands />
       <AboutSectionOne />
       <AboutSectionTwo />
-      <Testimonials />
-      <Pricing />  
-      <Blog />
+      {/* <Testimonials /> */}
+      {/* <Pricing />   */}
+      {/* <Blog /> */}
       <Contact />
     </>
   );
